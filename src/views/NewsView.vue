c03c119c6b080a057626e3ac63764d10e8d3c8ce
zero-stealth <penguincipher@gmail.com>
2023-05-13 12:58:33 +0300

    'stable'

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,7 +1,13 @@
 <script setup>
+import desktopNav from '@/components/desktopNav.vue'
 import mobileNav from '@/components/mobileNav.vue'
 </script>
 <template>
+  <div>
+    <div class="desknav-layout">
+      <desktopNav class="desk-show" />
+    </div>
+  </div>
   <div class="mobilenav-layout">
     <mobileNav />
   </div>
